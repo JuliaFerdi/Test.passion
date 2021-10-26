39482d23c323c0ba96b86e172f83400dddbd3d2c
JuliaFerdi <ferdimonova@gmail.com>
2021-10-26 09:24:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+async function example(){
+
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get("https://passionate.io/build-an-app/name")
+    
+    await driver.findElement(By.name("appName")).sendKeys("Sunny day", Key.RETURN);
+     //await driver.quit();
+}
+
+example()
